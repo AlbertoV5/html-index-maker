17eae81a404ce9a2b4ad43dde76f18e84f584f5c
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-11-20 19:06:04 -0600

    First working version

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,58 @@
 import subprocess
 import logging
+import json
+
 
 log = logging.getLogger(__name__)
 
 
-def test_main():
-    command = ["poetry", "run", "python", "-m", "html-index-maker", "Bob"]
-    out = subprocess.check_output(command)
-    assert out.decode("utf-8") == "Hello Bob\n"
-    log.debug(out)
+def test_file():
+    command = [
+        "poetry",
+        "run",
+        "python",
+        "-m",
+        "html-index-maker",
+        "./resources/ml.html",
+    ]
+    log.debug(command)
+    subprocess.run(command)
+    with open("./data.json", "r") as json_file:
+        data = json.loads(json_file.read())
+    first_header = {
+        "tag": "h2",
+        "id": "machine-learning",
+        "text": "1. Machine Learning",
+    }
+    second_header = {
+        "tag": "h3",
+        "id": "supervised-learning",
+        "text": "1.1. Supervised Learning",
+    }
+    assert data[0]["headers"][0] == first_header
+    assert data[0]["headers"][1] == second_header
+
+
+def test_directory():
+    command = [
+        "poetry",
+        "run",
+        "python",
+        "-m",
+        "html-index-maker",
+        "./resources",
+    ]
+    log.debug(command)
+    subprocess.run(command)
+    with open("./data.json", "r") as json_file:
+        data = json.loads(json_file.read())
+    for file in data:
+        if "/unsupervised-ml.html" in file["file"]:
+            assert file["headers"][0]["id"] == "unsupervised-machine-learning"
+            assert file["headers"][1]["id"] == "clustering"
+        if "/ml.html" in file["file"]:
+            assert file["headers"][0]["id"] == "machine-learning"
+            assert file["headers"][1]["id"] == "supervised-learning"
+        if "/deeplearning.html" in file["file"]:
+            assert file["headers"][0]["id"] == "deep-learning"
+            assert file["headers"][1]["id"] == "neural-networks"
